1df4304d0e7c2cd05526d1f1736d083b77893318
clair-uzh <clair981207@gmail.com>
2026-05-22 11:44:49 +0800

    1

---
+++ zou-no-hana-park/data.js
@@ -0,0 +1,203 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-zou-no-hana-park",
+      "name": "Zou-no-hana Park",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.4196482262024297,
+        "pitch": -0.05106590363321928,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.813756388329624,
+          "pitch": 0.050031228903602454,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -0.6004288261932551,
+          "pitch": 0.15573742198431972,
+          "rotation": 6.283185307179586,
+          "target": "1--"
+        },
+        {
+          "yaw": -1.5502202858058922,
+          "pitch": 0.07497790274623384,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.603605858381183,
+          "pitch": 0.42176082158843364,
+          "title": "EP4 21:47",
+          "text": "<p><br></p>"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "像鼻子 右",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.7563401521936406,
+        "pitch": 0.03967469937868984,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.509754678089392,
+          "pitch": 0.22941130734304238,
+          "rotation": 0,
+          "target": "0-zou-no-hana-park"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5687423015684914,
+          "pitch": 0.2693397447067092,
+          "title": "EP4 23:04",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2--",
+      "name": "像鼻子 左",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.74991718007122,
+        "pitch": -0.02457535775827857,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6261375376530474,
+          "pitch": 0.35825082163602673,
+          "rotation": 11.780972450961727,
+          "target": "0-zou-no-hana-park"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7505554406741597,
+          "pitch": 0.20635993311593204,
+          "title": "DEAD APPLE 28:10",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3--",
+      "name": "象鼻子 上",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.5187065009444405,
+        "pitch": 0.06606456120750615,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.586470341245354,
+          "pitch": 0.8095102839974508,
+          "rotation": 0,
+          "target": "0-zou-no-hana-park"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.49623943910159,
+          "pitch": 0.5337470921010752,
+          "title": "EP8 6:06",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Zou-no-hana Park",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
